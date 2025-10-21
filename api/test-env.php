4dbd719bfebb99fb0982ff5a2f1409d07a14709d
kayla168 <kaylazhang18@gmail.com>
2025-10-21 21:08:13 +0800

    Fix: Switch to the correct and maintained vercel-php runtime

---
+++ api/test-env.php
@@ -0,0 +1,9 @@
+<?php
+// /api/test-env.php
+
+// 設定響應頭為 JSON 格式，方便查看
+header('Content-Type: application/json');
+
+// 將 $_SERVER 超全局變數中的所有內容，以 JSON 格式打印出來
+echo json_encode($_SERVER, JSON_PRETTY_PRINT);
+?>
